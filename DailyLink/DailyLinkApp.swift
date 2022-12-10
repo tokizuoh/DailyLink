da23710ce4e1f26aed398503af9f51efade746cc
tokizuoh <tokizuoh@gmail.com>
2022-12-10 13:33:31 +0900

    init

---
+++ DailyLink/DailyLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyLinkApp.swift
+//  DailyLink
+//
+//  Created by tokizo on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct DailyLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
